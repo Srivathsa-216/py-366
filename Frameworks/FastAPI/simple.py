6865d026bf0fb880f111c76fe40263f76183999c
Adarsh srivathsa M S <srivathsa216@gmail.com>
2024-03-07 07:53:27 +0530

    Developing GET API in fastAPI

--- Frameworks/FastAPI/simple.py
+++ Frameworks/FastAPI/simple.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
-
-# app = FastAPI()
-
-# @app.get("/hello/{name}")
-# async def hello(name):
-#     return f"Welcome to the FastAPI world!!!, {name}"
+from enum import Enum
 
 app = FastAPI()
+
+@app.get("/hello/{name}")
+async def hello(name):
+    return f"Welcome to the FastAPI world!!!, {name}"
+
+food_items = {
+    'Indian': ['Dosa', 'Ragiball'],
+    'American': ['Hotdog', 'Apple Pie'],
+    'Italian': ['Ravioli', 'Pizza']
+}
+
+class AvailableCuisines(str, Enum):
+    Indian = "Indian"
+    American = "American"
+    Italian = "Italian"
+
+@app.get("/fooditems/{cuisine}")
+async def get_items(cuisine: AvailableCuisines):
+    return food_items.get(cuisine)
