330e0acdeee0bfb05c8947eb3dd45b4015ac881d
Adarsh srivathsa M S <srivathsa216@gmail.com>
2024-05-28 20:17:57 +0530

    CRUD Operations and Project structure change

--- Mini Projects/Backend - FastAPI/Social media/posts/main.py
+++ Mini Projects/Backend - FastAPI/Social media/posts/main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI
-from fastapi.params import Body
+from fastapi import FastAPI, status, HTTPException, Response
 
 from pydantic import BaseModel
 from typing import Optional
@@ -23,6 +22,11 @@ def find_post(id):
     for p in my_posts:
         if p["id"] == id:
             return p
+        
+def find_index_post(id):
+    for i,p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 @app.get("/")
 def root():
@@ -32,7 +36,7 @@ def root():
 def get_posts():
     return {"data": my_posts }
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
     print(post)
     post_dict = post.model_dump()
@@ -47,5 +51,38 @@ def create_post(post: Post):
 
 @app.get("/posts/{id}")
 def get_post(id: int):
-    post = find_post(id)
+
+    if not (post := find_post(id)):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id : {id} was not found")
+
     return {"post_detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"Post with id: {id} doesnt exist")
+
+    my_posts.pop(index)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"Post with id: {id} doesnt exist")
+    
+    post_dict = post.model_dump()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+
+    return {"Data Updated Succesfully" : post_dict}
